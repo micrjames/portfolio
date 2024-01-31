d8a576991a34b1ae9037df05651f257f35504950
Michael James <micrjames921@gmail.com>
2024-01-30 17:21:59 -0700

    got working modal open/close

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-requirejs(['js/modal.min.js', 'js/incs.js', 'js/modal.incs.js'], function() {
+requirejs(['js/modal.min.js', 'js/incs.js'], function() {
    const modal = new Modal(modalEl);
    contactInfoEmailMessage.addEventListener("click", () => {
 	  modal.open();
