9d459b5c31c308e0fe73793653a294a8724d2281
Michael James <micrjames921@gmail.com>
2024-01-30 18:37:47 -0700

    got working Modal and Form code; rewrote Form class s.t. textarea data is recorded as well

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
-requirejs(['js/modal.min.js', 'js/incs.js'], function() {
+requirejs(['js/modal.min.js', 'js/form.min.js', 'js/incs.js'], function() {
    const modal = new Modal(modalEl);
    contactInfoEmailMessage.addEventListener("click", () => {
 	  modal.open();
+
+	  const form = new Form(modal.body.children[0], data => {
+		  console.log(data);
+	  });
    });
 });
